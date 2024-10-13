80ac898cbb1b4bae9d54d5f0cbddfeb9de82a850
Leyman Maxim <maximleyman@yandex.ru>
2024-10-13 21:00:47 +0300

    refactoring + change preview update logic

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use std::{cell::RefCell, io::Cursor};
 use std::error::Error;
 use std::time::Duration;
 use std::rc::Rc;
+use std::sync::atomic::{AtomicBool, Ordering};
 
-use serialport::{available_ports, new, SerialPortType};
-use slint::{Model, SharedPixelBuffer, VecModel, Weak};
+use ring_channel::{ring_channel, RingSender};
+use serialport::{available_ports, SerialPortType};
+use slint::{Image, Rgba8Pixel, Model, RenderingState, SharedPixelBuffer, VecModel, Weak};
 use windows_capture::{
     capture::{GraphicsCaptureApiHandler, CaptureControl},
     monitor::Monitor, settings::{ColorFormat, Settings},
@@ -20,20 +22,16 @@ use screen_capture::Capture;
 
 slint::include_modules!();
 
-const MAX_TITLE_LEN: usize = 32;
-
 type CaptureControlRef = Rc<RefCell<Option<CaptureControl<Capture, Box<dyn Error + Send + Sync>>>>>;
+
+static CAPTURING_ENABLED: AtomicBool = AtomicBool::new(false);
 
 
 fn get_monitor_title(monitor: &Monitor) -> String {
     let name = monitor.name().expect("Can't get monitor title");
     let device_str = monitor.device_string().expect("Can't get device string from monitor");
 
-    let mut title = format!("{} ({})", name, device_str);
-    if title.chars().count() > MAX_TITLE_LEN {
-        title = title.chars().take(MAX_TITLE_LEN).collect::<String>();
-        title.push_str("...");
-    }
+    let title = format!("{} ({})", name, device_str);
     title
 }
 
@@ -73,45 +71,57 @@ fn start_com_ports_observer(main_window_weak: Weak<MainWindow>) {
 }
 
 
-fn start_monitors_observer(main_window_weak: Weak<MainWindow>) {
+fn start_monitors_observer(main_window_weak: Weak<MainWindow>, tx: RingSender<(i32, SharedPixelBuffer<Rgba8Pixel>)>) {
     let image_bytes = include_bytes!("../monitor-icon.jpg");
     let img: DynamicImage = ImageReader::new(Cursor::new(image_bytes)).with_guessed_format().unwrap().decode().unwrap();
     let pix_buff = SharedPixelBuffer::clone_from_slice(&img.to_rgba8(), img.width(), img.height());
-    
+    let tx = tx.clone();
     std::thread::spawn(move || {
-        let mut handlers = Vec::new();
+        let mut handlers: Vec<CaptureControl<Preview, Box<dyn Error + Send + Sync>>> = Vec::new();
+        let stop_all_previews = |handlers: &mut Vec<CaptureControl<Preview, Box<dyn Error + Send + Sync>>>| {
+            while let Some(handler) = handlers.pop() {
+                handler.stop().unwrap();
+            }
+        };
 
         loop {
-            let monitors: Vec<Monitor> = Monitor::enumerate().unwrap();
-
-            if monitors.len() == handlers.len() && handlers.iter().all(|h: &CaptureControl<Preview, Box<dyn Error + Send + Sync>>| !h.is_finished()) {
+            if CAPTURING_ENABLED.load(Ordering::Relaxed) {
+                stop_all_previews(&mut handlers);
                 std::thread::sleep(Duration::from_millis(500));
                 continue;
             }
 
-            for handler in handlers.into_iter() {
-                handler.stop().unwrap();
+            let monitors: Vec<Monitor> = Monitor::enumerate().unwrap();
+            let all_handlers_active = handlers.iter().all(|h: &CaptureControl<Preview, Box<dyn Error + Send + Sync>>| !h.is_finished());
+
+            if monitors.len() == handlers.len() && all_handlers_active {
+                std::thread::sleep(Duration::from_millis(500));
+                continue;
             }
 
-            handlers = Vec::new();
+            stop_all_previews(&mut handlers);
+
             let pix_buff = pix_buff.clone();
             
-            let monitors_copy = monitors.clone();
-            let _ = main_window_weak.upgrade_in_event_loop(move |w| {
-                let mon_mock = slint::Image::from_rgba8(pix_buff);
-                let monitors_vec = monitors_copy.iter().map(|m| {
-                    let title = get_monitor_title(m);
-                    MonitorData{
-                        title: title.into(),
-                        image: mon_mock.clone(),
-                    }
-                }).collect::<Vec<MonitorData>>();
-                let monitors_vec_model = Rc::new(VecModel::from(monitors_vec));
-                w.set_monitors_data(monitors_vec_model.into())
+            let _ = main_window_weak.upgrade_in_event_loop({
+                let monitors_titles: Vec<String> = monitors.iter().map(|m| get_monitor_title(m)).collect();
+                
+                move |w| {
+                    let mon_mock = slint::Image::from_rgba8(pix_buff);
+                    let monitors_vec = monitors_titles.iter().map(|title| {
+                        MonitorData{
+                            title: title.into(),
+                            image: mon_mock.clone(),
+                        }
+                    }).collect::<Vec<MonitorData>>();
+                    let monitors_vec_model = Rc::new(VecModel::from(monitors_vec));
+                    w.set_monitors_data(monitors_vec_model.into())
+                }
             });
 
             for (index, monitor) in monitors.into_iter().enumerate() {
-                let handler = start_monitor_preview(index, monitor, main_window_weak.clone());
+                let tx = tx.clone();
+                let handler = start_monitor_preview(index, monitor, tx);
                 handlers.push(handler);
             }
         }
@@ -119,9 +129,12 @@ fn start_monitors_observer(main_window_weak: Weak<MainWindow>) {
 }
 
 
-fn start_monitor_preview(index: usize, monitor: Monitor, main_window_weak: Weak<MainWindow>) -> CaptureControl<Preview, Box<dyn Error + Send + Sync>> {
-    let title = get_monitor_title(&monitor);
-    let flags = (title, index, main_window_weak);
+fn start_monitor_preview(
+    index: usize,
+    monitor: Monitor,
+    tx: RingSender<(i32, SharedPixelBuffer<Rgba8Pixel>)>) -> CaptureControl<Preview, Box<dyn Error + Send + Sync>> 
+{
+    let flags = (index, tx);
 
     let settings = Settings::new(
         monitor,
@@ -140,8 +153,41 @@ fn main() {
 
     let capture_control: CaptureControlRef = Rc::new(RefCell::new(None));
 
+    let (tx, rx) = ring_channel(std::num::NonZeroUsize::try_from(1).unwrap());
+
+    // May be usefull if we want more smoothely preview update without debug flags
+    // Takes more CPU resources
+    // std::thread::spawn({
+    //     let main_weak = main_window.as_weak();
+    //     move || {
+    //         loop {
+    //             if let Ok((index, monitor_img)) = rx.try_recv() {
+    //                 main_weak.upgrade_in_event_loop(move |mw| {
+    //                     mw.invoke_update_monitor_preview(index, Image::from_rgba8(monitor_img));
+    //                     mw.window().request_redraw()
+    //                 }).unwrap();
+    //             }
+    //         }
+    //     }
+    // });
+
+    main_window.window().set_rendering_notifier({
+        let main_weak = main_window.as_weak();
+        move |state, _api| {
+            match state {
+                RenderingState::BeforeRendering => {
+                    if let (Some(mw), Ok((index, monitor_img))) = (main_weak.upgrade(), rx.try_recv()) {
+                        mw.invoke_update_monitor_preview(index, Image::from_rgba8(monitor_img));
+                        mw.window().request_redraw()
+                    }
+                },
+                _ => {}
+            }
+        }
+    }).unwrap();
+
     start_com_ports_observer(main_window.as_weak());
-    start_monitors_observer(main_window.as_weak());
+    start_monitors_observer(main_window.as_weak(), tx);
   
     let capture_control_clone = capture_control.clone();
     
@@ -155,6 +201,7 @@ fn main() {
                 if !capture_control.is_finished() {
                     capture_control.stop().unwrap();
                     main_window.set_is_capture_running(false);
+                    CAPTURING_ENABLED.store(false, Ordering::Relaxed);
                     return
                 }
             }
@@ -164,7 +211,7 @@ fn main() {
             let selected_monitor_idx = main_window.get_selected_monitor() as usize;
             let monitor = Monitor::from_index(selected_monitor_idx).unwrap();
 
-            let com_port_builder = new(selected_com_port.to_string(), 250000);
+            let com_port_builder = serialport::new(selected_com_port.to_string(), 250000);
             let mut com_port = com_port_builder.open().unwrap();
             com_port.set_timeout(Duration::from_millis(100)).unwrap();
 
@@ -179,6 +226,7 @@ fn main() {
             main_window.set_is_capture_running(true);
             let capture_control: CaptureControl<Capture, Box<dyn Error + Send + Sync>> = Capture::start_free_threaded(settings).unwrap();
             *capture_lock = Some(capture_control);
+            CAPTURING_ENABLED.store(true, Ordering::Relaxed);
         }
     });
 
